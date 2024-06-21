a239df3f045eb873b062273e56b99e8152555553
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-06-21 22:11:03 +0530

    Remote Driver Chrome Session Issue Fixed

--- src/main/java/com/automation/SpringConcepts/configuration/RemoteWebDriverConfiguration.java
+++ src/main/java/com/automation/SpringConcepts/configuration/RemoteWebDriverConfiguration.java
@@ -2,31 +2,23 @@ package com.automation.SpringConcepts.configuration;
 
 import org.springframework.context.annotation.Configuration;
 
-import org.openqa.selenium.ImmutableCapabilities;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
-import java.time.Duration;
 
-//@Lazy
+@Lazy
 @Configuration
 @Profile("remote")
 public class RemoteWebDriverConfiguration {
@@ -34,40 +26,41 @@ public class RemoteWebDriverConfiguration {
     @Value("${selenium.grid.url}")
     private String url;
 
-    @Autowired
-    //private DesiredCapabilities desiredCapabilities;
+    @Value("${browser}")
+    private String browser;
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
 
     @Bean
     @ConditionalOnProperty(name = "browser", havingValue = "firefox")
     public WebDriver remoteFirefoxDriver() {
         System.setProperty("webdriver.http.factory", "jdk-http-client");
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setPlatform(Platform.ANY);
-        desiredCapabilities.setBrowserName("Firefox");
+        capabilities.setPlatform(Platform.ANY);
+        capabilities.setBrowserName("Firefox");
         FirefoxOptions options = new FirefoxOptions();
         // #14125 - Selenium 4.21.0 version bug - Unable to launch session with Grid without below parameter
         options.setEnableDownloads(true);
-        options.merge(desiredCapabilities);
+        options.merge(capabilities);
         try {
-            return new RemoteWebDriver(URI.create(url).toURL(), desiredCapabilities);
+            return new RemoteWebDriver(URI.create(url).toURL(), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Bean
-    //@ConditionalOnMissingBean
-    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    @ConditionalOnMissingBean
     public WebDriver remoteChromeDriver() throws MalformedURLException {
+        ChromeOptions chromeOptions = null;
         System.setProperty("webdriver.http.factory", "jdk-http-client");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setPlatform(Platform.WINDOWS);
-        capabilities.setBrowserName("Chrome");
-        ChromeOptions chromeOptions = new ChromeOptions();
+        capabilities.setBrowserName(browser);
+        chromeOptions = new ChromeOptions();
         // Selenium 4.21.0 version bug - Unable to launch session with Grid without below parameter
         chromeOptions.setEnableDownloads(true);
         chromeOptions.merge(capabilities);
-        return new RemoteWebDriver(URI.create(url).toURL(), capabilities);
+        return new RemoteWebDriver((URI.create(url).toURL()), capabilities);
+
     }
 
 }
