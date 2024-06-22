571acee9a8e844f4ae48088d9f93454ac5291533
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-06-22 09:42:02 +0530

    Selenium Grid Browser Session Launch Issue Fixed. DryRun for Chrome, Firefox and Edge is Working Fine.

---
+++ src/main/java/com/automation/SpringConcepts/configuration/RemoteDriverConfiguration.java
@@ -0,0 +1,63 @@
+package com.automation.SpringConcepts.configuration;
+
+import org.openqa.selenium.edge.EdgeOptions;
+import org.springframework.context.annotation.Configuration;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Profile;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+@Lazy
+@Configuration
+@Profile("remote")
+public class RemoteDriverConfiguration {
+
+    @Value("${selenium.grid.url}")
+    private String url;
+
+    @Value("${browser}")
+    private String browser;
+
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+
+    @Bean
+    public WebDriver remoteDriver() throws MalformedURLException {
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.http.factory", "jdk-http-client");
+            capabilities.setPlatform(Platform.WINDOWS);
+            capabilities.setBrowserName(browser);
+            ChromeOptions chromeOptions = new ChromeOptions();
+            // Selenium 4.21.0 version bug - Unable to launch session with Grid without below parameter
+            chromeOptions.setEnableDownloads(true);
+            chromeOptions.merge(capabilities);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            System.setProperty("webdriver.http.factory", "jdk-http-client");
+            capabilities.setPlatform(Platform.WINDOWS);
+            capabilities.setBrowserName(browser);
+            FirefoxOptions options = new FirefoxOptions();
+            // Selenium 4.21.0 version bug - Unable to launch session with Grid without below parameter
+            options.setEnableDownloads(true);
+            options.merge(capabilities);
+        } else if (browser.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.http.factory", "jdk-http-client");
+            capabilities.setPlatform(Platform.WINDOWS);
+            capabilities.setBrowserName("MicrosoftEdge");
+            EdgeOptions options = new EdgeOptions();
+            // Selenium 4.21.0 version bug - Unable to launch session with Grid without below parameter
+            options.setEnableDownloads(true);
+            options.merge(capabilities);
+        }
+        return new RemoteWebDriver((URI.create(url).toURL()), capabilities);
+    }
+
+}
--- src/test/java/com/automation/SpringConcepts/google/GoogleApp1Test.java
+++ src/test/java/com/automation/SpringConcepts/google/GoogleApp1Test.java
@@ -3,10 +3,13 @@ package com.automation.SpringConcepts.google;
 import com.automation.SpringConcepts.SpringTestNGTest;
 import com.automation.SpringConcepts.page.google.GooglePage;
 import com.automation.SpringConcepts.util.ScreenCapture;
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class GoogleApp1Test extends SpringTestNGTest {
 
@@ -21,6 +24,7 @@ public class GoogleApp1Test extends SpringTestNGTest {
     public void validateGoogleTest() {
         this.googlePage.launch();
         Assert.assertTrue(this.googlePage.isAt());
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.SECONDS);
         this.googlePage.getSearch().search("Spring Boot");
         Assert.assertTrue(this.googlePage.getSearchResult().isAt());
         Assert.assertTrue(this.googlePage.getSearchResult().getResultCount() > 10);
