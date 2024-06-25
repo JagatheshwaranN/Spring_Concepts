364816bf65884257493ecb12af9c604639715737
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2024-06-25 09:05:57 +0530

    Cucumber BDD Approach Implementation In-progress. Facing issue while running the Cucumber Scenario.

---
+++ src/test/java/com/automation/SpringConcepts/steps/GoogleSteps.java
@@ -0,0 +1,36 @@
+package com.automation.SpringConcepts.steps;
+
+import com.automation.SpringConcepts.core.annotation.LazyAutowired;
+import com.automation.SpringConcepts.page.google.GooglePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.Assert;
+
+@SpringBootTest
+@CucumberContextConfiguration
+public class GoogleSteps {
+
+    @LazyAutowired
+    GooglePage googlePage;
+
+    @Given("I am on the google home page")
+    public void launchGoogleSite() {
+        this.googlePage.launch();
+        Assert.assertTrue(this.googlePage.isAt());
+    }
+
+    @When("I type {string} and click on search button")
+    public void searchOnGoogle(String keyword) {
+        this.googlePage.getSearch().search(keyword);
+        Assert.assertTrue(this.googlePage.getSearch().isAt());
+    }
+
+    @Then("I should see at least {int} search results")
+    public void verifySearchResult(int count) {
+        Assert.assertTrue(this.googlePage.getSearchResult().getResultCount() >= count);
+    }
+
+}
---
+++ src/test/resources/features/GoogleSearch.feature
@@ -0,0 +1,12 @@
+Feature: Google Search Functionality
+
+  Scenario Outline: As a user, I want to search on the google site
+    Given I am on the google home page
+    When I type "<keyword>" and click on search button
+    Then I should see at least <count> search results
+
+    Examples:
+    | keyword     | count |
+    | Spring Boot | 5     |
+    | Selenium    | 3     |
+    | Java        | 2     |
